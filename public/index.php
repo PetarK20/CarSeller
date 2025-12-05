20bec04b44710d2a8e005ee4ba3f77ef68d4810a
Petar Kolev <148004863+PetarK20@users.noreply.github.com>
2025-12-05 18:01:03 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,18 +2,11 @@
 // public/index.php
 session_start();
 
-// Начален PHP таг.
 require_once '../app/models/CarModel.php';
-
-// Включва файла с модела CarModel (работи с данните).
 require_once '../app/controllers/CarController.php';
-
-// Включва файла с контролера CarController (обработва публични заявки).
 require_once '../app/controllers/AdminController.php';
 
-// Включва файла с контролера AdminController (обработва административни заявки).
 
-// --- ROUTER ---
 // Взима стойността на GET параметъра 'url' или празен низ.
 $url = $_GET['url'] ?? '';
 
@@ -22,7 +15,6 @@ $parts = explode('/', trim($url, '/'));
 
 
 $route = ($parts[0] ? $parts[0] : 'public');
-
 
 // Първата част е 'маршрут' (route), по подразбиране 'public'.
 // print_r("Route: " . $route . "\n");
@@ -33,30 +25,17 @@ $id = $parts[2] ?? null;
 // Третата част е 'идентификатор' (ID), по подразбиране null.
 
 // Избор на контролер
-// Коментар, указващ, че следва логиката за избор на контролер.
 switch ($route) {
-    // Започва проверка на стойността на $route.
     case 'admin':
-        // Ако $route е 'admin'.
-
         $controller = new AdminController(new CarModel());
-
-
-        // Създава се нов обект от класа AdminController.
         break;
-    // Прекратява switch блока.
     case 'public':
-        // Ако $route е 'public'.
     default:
-        // Ако $route не е нито 'admin', нито 'public' (или е 'public').
         $controller = new CarController();
-        // По подразбиране се създава нов обект от класа CarController.
         break;
-        // Прекратява switch блока.
 }
 
-// Изпълняваме метода
-// Коментар, указващ, че следва логиката за изпълнение на метода/действието.
+
 if (method_exists($controller, $action)) {
     // Проверява дали методът ($action) съществува в избрания контролер.
     $id ? $controller->$action($id) : $controller->$action();
@@ -66,3 +45,4 @@ if (method_exists($controller, $action)) {
     $controller->index();
     // Извиква се методът 'index' като резервен вариант.
 }
+
